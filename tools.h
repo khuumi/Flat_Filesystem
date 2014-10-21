a3c149fcc44af63daa92ee54225d94bbf80626d4
Daniel Sadik <sadik@Bukendur.local>
2014-10-20 21:59:22 -0400

    added yet another

--- tools.h
+++ tools.h
@@ -15,6 +15,9 @@ int valid_group(string group);
 int validate_acl(string line);
 int sanitize(string to_sanitize, int type);
 int is_underscore(char c);
-
+int check_acl(ifstream& file_to_open, 
+	string user_name, 
+	string group_name, 
+	string access);
 
 
