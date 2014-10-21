2aa6d3479f658b698861b5d57f4a0edd12e1f92a
Daniel Sadik <sadik@Bukendur.local>
2014-10-21 11:23:49 -0400

    fixing bugs

--- tools.cpp
+++ tools.cpp
@@ -189,7 +189,7 @@ int check_acl(ifstream& file_to_open,
 			if (user_name == user || user == "*"){
 				if(group_name == group || group == "*"){
 					// cout << ops.find(access) << endl;	
-					if(ops.find(access) >= 0 )
+					if(ops.find(access) != npos )
 						to_return = 1;
 				}
 			}
