d7bf90ed188b68a1144e7957330846bd8872e612
Daniel Sadik <sadik@Bukendur.local>
2014-11-12 22:40:50 -0500

    casting

--- testruns/new.cpp
+++ testruns/new.cpp
@@ -35,14 +35,14 @@ int decrypt( char *ciphertext, int ciphertext_len, char *key,
   /* Provide the message to be decrypted, and obtain the plaintext output.
    * EVP_DecryptUpdate can be called multiple times if necessary
    */
-  if(1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len))
+  if(1 != EVP_DecryptUpdate(ctx, reinterpret_cast<const unsigned char *>(&plaintext), &len, reinterpret_cast<const unsigned char*> (&ciphertext), ciphertext_len))
     handleErrors();
   plaintext_len = len;
 
   /* Finalise the decryption. Further plaintext bytes may be written at
    * this stage.
    */
-  if(1 != EVP_DecryptFinal_ex(ctx, plaintext + len, &len)) handleErrors();
+  if(1 != EVP_DecryptFinal_ex(ctx, reinterpret_cast<const unsigned char *>(&(plaintext + len)), &len)) handleErrors();
   plaintext_len += len;
 
   /* Clean up */
@@ -70,20 +70,20 @@ int encrypt(char *plaintext, int plaintext_len,char *key,
    * In this example we are using 256 bit AES (i.e. a 256 bit key). The
    * IV size for *most* modes is the same as the block size. For AES this
    * is 128 bits */
-  if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
+  if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, reinterpret_cast<const unsigned char *>(&key), reinterpret_cast<const unsigned char *>(&iv)))
     handleErrors();
 
   /* Provide the message to be encrypted, and obtain the encrypted output.
    * EVP_EncryptUpdate can be called multiple times if necessary
    */
-  if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
+  if(1 != EVP_EncryptUpdate(ctx,reinterpret_cast<const unsigned char *>(&ciphertext), &len, reinterpret_cast<const unsigned char *>(&plaintext), plaintext_len))
     handleErrors();
   ciphertext_len = len;
 
   /* Finalise the encryption. Further ciphertext bytes may be written at
    * this stage.
    */
-  if(1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len)) handleErrors();
+  if(1 != EVP_EncryptFinal_ex(ctx, reinterpret_cast<const unsigned char *>(&(plaintext + len)), &len)) handleErrors();
   ciphertext_len += len;
 
   /* Clean up */
